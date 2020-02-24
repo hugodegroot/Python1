8c4ae32bb662b2d6d689068e8821997aaeb063b4
Hugo de Groot <hugo.de.groot@hva.nl>
2020-02-24 23:05:35 +0100

    Updated

--- client.py
+++ client.py
@@ -1,78 +1,60 @@
 # Python program to implement client side of chat room. 
 import socket 
-import select 
-import sys 
+import select
 import threading
-import random
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.connect(("18.195.107.195", 5378))
 
-if len(sys.argv) != 3: 
-    print("Correct usage: script, IP address, port number")
-    exit() 
+def connectSocket():
+    sock.connect(("18.195.107.195", 5378))
+    return sock
 
-IP_address = str(sys.argv[1]) 
-Port = int(sys.argv[2]) 
-s.connect((IP_address, Port))
+def makeUser(sock):
+    username = input("Username: ")
+    message = "HELLO-FROM " + username + '\n'
+    sock.send(message.encode("utf-8"))
+    data = sock.recv(4096)
+    if "IN-USE".encode("utf-8") in data:
+        return False
+    else: 
+        print(data)
+        return True
 
-def getData():
+def getServerData():
     while True:
-        try:
-            data = s.recv(4096)
-            print(data)
-            if "IN-USE".encode("utf-8") in data:
-                s.close()
-                makeUserAgain()
-            if not data : 
-                print("Socket is closed.")
-                break
-            else : print(data.decode("utf-8"))
-        except OSError as msg:
-            print(msg)
-            continue
-        
+        data = sock.recv(4096)
+        if not data : 
+            print("Socket is closed.")
+            break
+        else : print(data.decode("utf-8"))
 
-t1 = threading.Thread(target=getData)
-t1.start()
-
-def makeUser():
-    my_username = input("Username: ")
-    username = my_username
-    message = "HELLO-FROM " + username + '\n'
-    s.send(message.encode("utf-8"))
-
-def makeUserAgain():
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect(("18.195.107.195", 5378))
-    my_username = input("Username: ")
-    username = my_username
-    message = "HELLO-FROM " + username + '\n'
-    s.send(message.encode("utf-8"))
-    
-makeUser()
-
-# message = "WHO\n"
-# s.send(message.encode("utf-8"))
-# users_list = s.recv(4096)
-# print(users_list)
-# for x in users_list:
-#     if x == my_username:
-#         my_username = input("This username has already been taken, please try another one: ")
-#     else:
-#         continue
+def getUserData():
+    while True:
+        val = input("input: ")
+        if val == "!quit": 
+            sock.close()
+            break
+        elif val == "!who":
+            message = "WHO\n"
+            sock.send(message.encode())
+        elif "@" in val:
+            val = val.replace("@", "SEND ")
+            message = val + "\n"
+            sock.send(message.encode())
+        else:
+            val = input("")
 
 while True:
-    val = input("")
-    if val == "!quit": 
-        s.close()
-        break
-    elif val == "!who":
-        message = "WHO\n"
-        s.send(message.encode())
-    elif "@" in val:
-        val = val.replace("@", "SEND ")
-        message = val + "\n"
-        s.send(message.encode())
+    userMade = makeUser(sock)
+    if userMade:
+        t1 = threading.Thread(target=getServerData())
+        t2 = threading.Thread(target=getUserData())
+        t1.start()
+        t2.start()
     else:
-        val = input("")
+        sock.close()
+        connectSocket()
+
 t1.join()
+t2.join()
