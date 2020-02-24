bc3b5676d0ed701851ab48da8e81fd13fbeca52b
Hugo de Groot <hugo.de.groot@hva.nl>
2020-02-24 23:57:07 +0100

    third try

--- client.py
+++ client.py
@@ -4,11 +4,11 @@ import select
 import threading
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect(("18.195.107.195", 5378))
 
-def connectSocket():
-    sock.connect(("18.195.107.195", 5378))
-    return sock
+def connectSocket(s):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect(("18.195.107.195", 5378))
+    return s
 
 def makeUser(sock):
     username = input("Username: ")
@@ -22,39 +22,52 @@ def makeUser(sock):
         return True
 
 def getServerData():
-    while True:
-        data = sock.recv(4096)
-        if not data : 
-            print("Socket is closed.")
-            break
-        else : print(data.decode("utf-8"))
+    try:
+        while True:
+            data = sock.recv(4096)
+            if not data : 
+                print("Socket is closed.")
+                break
+            else : print(data.decode("utf-8"))
+    except OSError as msg:
+        print(msg)
 
 def getUserData():
-    while True:
-        val = input("input: ")
-        if val == "!quit": 
-            sock.close()
-            break
-        elif val == "!who":
-            message = "WHO\n"
-            sock.send(message.encode())
-        elif "@" in val:
-            val = val.replace("@", "SEND ")
-            message = val + "\n"
-            sock.send(message.encode())
-        else:
-            val = input("")
+    try:
+        while True:
+            val = input("input: ")
+            if val == "!quit":
+                sock.close()
+                break
+            if val == "!who":
+                message = "WHO\n"
+                sock.send(message.encode())
+                continue
+            if "@" in val:
+                val = val.replace("@", "SEND ")
+                message = val + "\n"
+                sock.send(message.encode())
+                continue
+            else:
+                continue
+    except OSError as msg:
+        print(msg)
 
 while True:
+    sock = connectSocket(sock)
     userMade = makeUser(sock)
     if userMade:
-        t1 = threading.Thread(target=getServerData())
-        t2 = threading.Thread(target=getUserData())
+        t1 = threading.Thread(target=getServerData)
+        t2 = threading.Thread(target=getUserData)
         t1.start()
         t2.start()
+        break
     else:
+        data = sock.recv(4096)
+        print(data)
+        sock.shutdown(1)
         sock.close()
-        connectSocket()
+        sock = connectSocket(sock)
 
 t1.join()
 t2.join()
